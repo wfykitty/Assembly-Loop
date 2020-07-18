bcf98a45a909e3dc3d9303b30acb66b5c400669a
wfykitty <feiya.wang@georgebrown.ca>
2020-07-18 17:03:06 -0400

    first commit

---
+++ test/test-bitwise.js
@@ -0,0 +1,29 @@
+const BitWise = artifacts.require('BitWise');
+const { BN } = require('@openzeppelin/test-helpers');
+const chaiBN = require('chai-bn')(BN); 
+require('chai').use(chaiBN);
+const { expect } = require('chai');
+
+contract("BitWise", () =>{
+    let bitwise;
+   before(() => {
+        return BitWise.deployed().then(instance => {
+            bitwise = instance;
+        })
+    })
+    it('countBitSet() should pass', async () => {
+        const myNumber = 250;
+        const gas = await bitwise.countBitSet.estimateGas(myNumber);
+        const gasAsm = await bitwise.countBitSetAsm.estimateGas(myNumber);
+        expect(gas).to.be.gt(gasAsm, "Assembly should be more gas efficient");
+
+        const result = await bitwise.countBitSet(myNumber);
+        const resultAsm = await bitwise.countBitSetAsm(myNumber);
+        expect(result).to.be.a.bignumber.that.equal(resultAsm, "result should match");
+    })
+
+    it('countBitSetAsm(0) should pass', async () => {
+        const resultAsm = await bitwise.countBitSetAsm(0);
+        assert.equal(resultAsm, 0, "result should match with 0");
+    });
+}) 
